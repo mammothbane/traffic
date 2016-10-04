4c2a577435ac04c0f02f36bd2a4c8d592f8989f4
Nathan Perry <itsamammoth@gmail.com>
2016-10-04 17:42:18 -0400

    try out Max heuristic

--- traffic/heuristic/__init__.py
+++ traffic/heuristic/__init__.py
@@ -1,7 +1,11 @@
+from .car_available_moves import CarAvailableMoves
 from .cars_in_between import CarsInBetween
 from .manhattan import Manhattan
+from .max import Max
 
 heuristics = {
     'cars_in_between': CarsInBetween,
     'manhattan': Manhattan,
+    'max': Max,
+    'car_available_moves': CarAvailableMoves,
 }
--- traffic/heuristic/max.py
+++ traffic/heuristic/max.py
@@ -1,8 +1,10 @@
-from . import heuristics
+from .car_available_moves import CarAvailableMoves
+from .cars_in_between import CarsInBetween
 from .heuristic import Heuristic
+from .manhattan import Manhattan
 
 
 class Max(Heuristic):
     @staticmethod
     def estimate(state):
-        return max([h.estimate(state) for h in heuristics.values()])
+        return max([h.estimate(state) for h in [CarAvailableMoves, CarsInBetween, Manhattan]])
